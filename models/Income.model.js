8ef2ff3ce346428ae5e90653e5fee7c773ab60f8
Muzna-J <muzna.jabban@gmail.com>
2023-05-10 14:19:03 +0200

    create models

---
+++ models/Income.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const IncomeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  currency: {
+    type: String,
+    required: true,
+    default: 'EUR' 
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  
+  description: {
+    type: String,
+    required: false
+  }
+});
+
+const Income = mongoose.model('Income', IncomeSchema);
+
+module.exports = Income;
